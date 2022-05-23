4e83018c55a02f08b1f79841aedb68ce9fc52dc4
NickCuso <hardlydiff@gmail.com>
2022-05-23 15:32:15 -0400

    Comments

--- scripts/rpc/getSaleHistory.ts
+++ scripts/rpc/getSaleHistory.ts
@@ -9,20 +9,24 @@ import { ReserveAuctionFinalizedEvent } from "../../typechain-types/FNDNFTMarket
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const addresses = require("@f8n/fnd-protocol/addresses.js");
 
+// The example NFT to query for, this should be updated with the NFT of interest
+const collection = "0x3B3ee1931Dc30C1957379FAc9aba94D1C48a5405";
+const tokenId = 105912;
+
 async function main(): Promise<void> {
+  // Connect to the Foundation market contract
   const market = FNDNFTMarket__factory.connect(addresses.prod[1].nftMarket, ethers.provider);
 
-  const collection = "0x3B3ee1931Dc30C1957379FAc9aba94D1C48a5405";
-  const tokenId = 105912;
-
+  // Query for all sales via Buy Price and via Offers
   const buyPricesAccepted = await market.queryFilter(market.filters.BuyPriceAccepted(collection, tokenId));
   const offersAccepted = await market.queryFilter(market.filters.OfferAccepted(collection, tokenId));
   // It is possible to query for all buyPricesAccepted and offersAccepted in a single request, but for simplicity we do it separately here
 
-  // Due to use of auctionIds, each auction listing needs to be checked independently
   const auctionsCreated = await market.queryFilter(market.filters.ReserveAuctionCreated(null, collection, tokenId));
+  // Due to use of auctionIds, each auction listing needs to be checked independently to see if it sold (and for how much)
   const auctionsFinalized: ReserveAuctionFinalizedEvent[] = [];
   for (const auction of auctionsCreated) {
+    // It is possible that an auction has ended but has not been finalized yet, and therefore be missed with this query
     const finalized = await market.queryFilter(market.filters.ReserveAuctionFinalized(auction.args.auctionId));
     if (finalized.length > 0) {
       // If the auction was finalized, that event will only be emitted once for the given auctionId
